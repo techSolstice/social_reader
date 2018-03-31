5e51ff46819111f696398e4bc4f8a68d67ef6cf5
Alex <alex@techsolstice.com>
2018-03-30 21:57:46 -0700

    Hey, I have a working twitter route with template and controller

--- src/Controller/SocialController.php
+++ src/Controller/SocialController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SocialController extends Controller
 {
+
     /**
      * @Route("/social", name="home")
      */
@@ -16,4 +17,31 @@ class SocialController extends Controller
             'controller_name' => 'SocialController',
         ]);
     }
+
+
+    /**
+     * @Route("/social/{medium}")
+     */
+    public function serve_medium($medium)
+    {
+        switch($medium)
+        {
+            case 'twitter':
+                $medium_response = $this->twitter_index();
+                break;
+            #@todo Account for invalid values and 404 or perhaps route back to main home
+        }
+
+        #@todo Is there a better way of returning a response that might not have been set?
+        if (!isset($medium_response)){return $this->index();}
+        return $medium_response;
+    }
+
+    public function twitter_index()
+    {
+        return $this->render('social/twitter.html.twig', [
+            'controller_name' => 'SocialController',
+        ]);
+    }
+
 }
